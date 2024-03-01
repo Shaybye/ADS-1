74d0f4dc566e133f198bd688c0d4223e1a20a7c5
Shaybye <159885796+Shaybye@users.noreply.github.com>
2024-03-01 21:22:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -37,7 +37,7 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   int sum = 0;
-  for (int i = 2; i < hbound; i++) {
+  for (int i = 2; i <= hbound; i++) {
       if (checkPrime(i)) {
           sum += i;
       }
