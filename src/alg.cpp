3429e8f93a67c96e3d9dedbcc30c4db8aae5e42e
Shaybye <159885796+Shaybye@users.noreply.github.com>
2024-03-01 17:21:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,43 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  int count = 0;
+  for (int i = 1; i <= value; i++) {
+      if (value % i == 0)
+          count++;
+  }
+  if (count == 2)
+      return true;
+  return false;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int c = 0;
+  for (int i = 1; ; i++) {
+      if (checkPrime(i))
+          c++;
+      if (c == n) {
+          return i;
+          break;
+      }
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  for (int i = value + 1; ; i++) {
+      if (checkPrime(i)) {
+          return i;
+          break;
+      }
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  int sum = 0;
+  for (int i = 2; i < hbound; i++) {
+      if (checkPrime(i)) {
+          sum += i;
+      }
+  }
+  return sum;
 }
